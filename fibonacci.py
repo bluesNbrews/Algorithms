91391b0ed83e1fe80dcfca3b165a106f9bb85c60
Steven <mstevenwilliams@gmail.com>
2019-12-06 17:40:39 -0500

    Improved algorithm.

--- fibonacci.py
+++ fibonacci.py
@@ -6,3 +6,12 @@ def getNthFib(n):
 		return 0
 	else:
 		return getNthFib(n - 2) + getNthFib(n - 1)
+
+#O(n) time | O(n) space
+#Utilize a hash map (dictionary in Python), initially passing in the dictionary with the first 2 values of the Fibonacci sequence
+def getNthFib(n, memoize = {2:1, 1:0}):
+	if n in memoize:
+		return memoize[n]
+	else:
+		memoize[n] = getNthFib(n - 2, memoize) + getNthFib(n - 1, memoize)
+    	return memoize[n]
